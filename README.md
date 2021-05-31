f5b13f73fc582b3b463a871579090db206dd3441
afozk95 <afozk95@gmail.com>
2021-05-31 20:24:38 +0300

    fix cli, add examples, more detailed readme

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # pgn-to-gif
 Generate GIFs of chess games from PGN files.
+
+## Installation
+`pip3 install -r requirements.txt`
+
+## Usage
+```console
+usage: main.py [-h] --pgn-path PGN_PATH --gif-path GIF_PATH [--add-initial-position {1,t,true,0,f,false}] [--highlight-last-move {1,t,true,0,f,false}]
+               [--orientation {white,black}] [--size SIZE] [--coordinates {1,t,true,0,f,false}] [--css-path CSS_PATH] [--loop LOOP] [--duration DURATION] [--fps FPS]
+               [--palettesize PALETTESIZE] [--subrectangles {1,t,true,0,f,false}]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --pgn-path PGN_PATH   path to pgn file to read (default: None)
+  --gif-path GIF_PATH   path to gif file to save (default: None)
+  --add-initial-position {1,t,true,0,f,false}
+                        add initial position to gif (default: true)
+  --highlight-last-move {1,t,true,0,f,false}
+                        highlight last move on board (default: true)
+  --orientation {white,black}
+                        orientation of board (default: white)
+  --size SIZE           size of board (default: 400)
+  --coordinates {1,t,true,0,f,false}
+                        add board coordinates (default: true)
+  --css-path CSS_PATH   path to css file to style board (default: None)
+  --loop LOOP           number of loops for gif, 0 means infinite (default: 0)
+  --duration DURATION   duration of each frame (in seconds) in gif (default: 1.0)
+  --fps FPS             frame per second of gif (default: 1.0)
+  --palettesize PALETTESIZE
+                        number of colors to quantize images to (default: 64)
+  --subrectangles {1,t,true,0,f,false}
+                        optimize gif by storing change (default: true)
+```
+
+## Examples
+Please see [examples](examples) folder.
--- main.py
+++ main.py
@@ -14,41 +14,56 @@ def parse_orientation(orientation: Literal["white", "black"]) -> chess.Color:
         raise ValueError(f"unknown {orientation=}")
 
 
+def parse_bool(bool_str: Literal["1", "t", "true", "0", "f", "false"]) -> bool:
+    true_values = ["1", "t", "true"]
+    false_values = ["0", "f", "false"]
+    if bool_str.lower() in true_values:
+        return True
+    elif bool_str.lower() in false_values:
+        return False
+    else:
+        raise ValueError(f"cannot parse to bool, {bool_str}")
+
+
 def main() -> None:
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--pgn-path", dest="pgn_path", required=True, type=Path, help="path to pgn file to read")
     parser.add_argument("--gif-path", dest="gif_path", required=True, type=Path, help="path to gif file to save")
-    parser.add_argument("--add-initial-postion", dest="add_initial_position", default=True, type=bool, help="add initial position to gif")
-    parser.add_argument("--highlight-last-move", dest="highlight_last_move", default=True, type=bool, help="highlight last move on board")
+    parser.add_argument("--add-initial-position", dest="add_initial_position", default="true", choices=["1", "t", "true", "0", "f", "false"], help="add initial position to gif")
+    parser.add_argument("--highlight-last-move", dest="highlight_last_move", default="true", choices=["1", "t", "true", "0", "f", "false"], help="highlight last move on board")
     parser.add_argument("--orientation", dest="orientation", default="white", choices=["white", "black"], help="orientation of board")
     parser.add_argument("--size", dest="size", default=400, type=int, help="size of board")
-    parser.add_argument("--coordinates", dest="coordinates", default=True, type=bool, help="add board coordinates")
+    parser.add_argument("--coordinates", dest="coordinates", default="true", choices=["1", "t", "true", "0", "f", "false"], help="add board coordinates")
     parser.add_argument("--css-path", dest="css_path", default=None, type=Path, help="path to css file to style board")
     parser.add_argument("--loop", dest="loop", default=0, type=int, help="number of loops for gif, 0 means infinite")
     parser.add_argument("--duration", dest="duration", default=1.0, type=float, help="duration of each frame (in seconds) in gif")
     parser.add_argument("--fps", dest="fps", default=1.0, type=float, help="frame per second of gif")
     parser.add_argument("--palettesize", dest="palettesize", default=64, type=int, help="number of colors to quantize images to")
-    parser.add_argument("--subrectangles", dest="subrectangles", default=True, type=bool, help="optimize gif by storing change")
+    parser.add_argument("--subrectangles", dest="subrectangles", default="true", choices=["1", "t", "true", "0", "f", "false"], help="optimize gif by storing change")
 
     args = parser.parse_args()
 
     pgn = read_pgn(args.pgn_path)
     style = None if args.css_path is None else read_css(args.css_path)
     orientation = parse_orientation(args.orientation)
+    add_initial_position = parse_bool(args.add_initial_position)
+    highlight_last_move = parse_bool(args.highlight_last_move)
+    coordinates = parse_bool(args.coordinates)
+    subrectangles = parse_bool(args.subrectangles)
     pgn_to_gif(
         pgn,
         args.gif_path,
-        args.add_initial_position,
-        args.highlight_last_move,
+        add_initial_position,
+        highlight_last_move,
         orientation,
         args.size,
-        args.coordinates,
+        coordinates,
         style,
         args.loop,
         args.duration,
         args.fps,
         args.palettesize,
-        args.subrectangles,
+        subrectangles,
     )
 
 
